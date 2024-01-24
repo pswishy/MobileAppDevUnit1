da6e7e95114d7db4ac254096f74b885b90884560
pswishy <piercemedy24@gmail.com>
2024-01-23 20:41:14 -0500

    change label constraint width to 300

--- MyProject/SakomotoViewController.swift
+++ MyProject/SakomotoViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapInfo(_ sender: UITapGestureRecognizer) {
+        if let tappedView = sender.view {
+            performSegue(withIdentifier: "detailSegue", sender: tappedView)
+        }
+    }
+    
 }
 
